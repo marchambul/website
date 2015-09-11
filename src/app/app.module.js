a84f45120c5b6b04a5712adb224e73fa3c9a790e
cymace <github@cymace.com>
2015-09-11 09:17:29 +0200

    automatic commit

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,3 +1,30 @@
 angular.module("marchambul", [
-
+  'ui.router',
+  "ui.bootstrap.tpls",
+  "ui.bootstrap.transition",
+  "ui.bootstrap.collapse",
+  'ui.bootstrap.position',
+  'ui.bootstrap.bindHtml',
+  'ui.bootstrap.typeahead',
+  'ui.bootstrap.modal',
+  'ui.bootstrap.tooltip',
+  'ui.bootstrap.position',
+  'ui.bootstrap.bindHtml',
+  'ui.bootstrap.popover'
 ])
+
+.config(($stateProvider)=>{
+
+    //routes config
+    $stateProvider
+    .state('account', {
+      url: "/account",
+      onEnter: ($state)=>{
+        console.log("test");
+      },
+      params:{configId: null},
+      templateUrl: "account/account.html",
+      controller: "AccountCtrl"
+    });
+  }
+)
