29b9001018667463611cb3534bc4ba2deadce7f5
cymace <github@cymace.com>
2015-11-08 13:07:22 +0100

    tournees home page

--- src/home/map/map.main.controller.js
+++ src/home/map/map.main.controller.js
@@ -1,21 +1,51 @@
 class MapCtrl{
-  constructor($scope, $http){
-    var map, marchambulIcon;
-    L.mapbox.accessToken = 'pk.eyJ1IjoibWFyY2hhbWJ1bCIsImEiOiI5NzI1MTA5ODk4MjFlM2EwZWIwM2Y4N2JiMjI5NTE4ZiJ9.2SFijYH9C8e_tZ2WyWboEQ';
-
-    marchambulIcon = L.icon({
-      iconUrl: 'marchambul-icon.png'
-    });
+    constructor($scope, $http){
+        var map, marchambulIcon;
 
 
-    this._init = function(){
+        this._init = function(){
 
-        // Create a map in the div #map
-        var map = L.mapbox.map('map', 'marchambul.6b67e8ac')
-        .setView([48.109, -1.67], 18);
-    };
-    this._init();
-  }
+            L.mapbox.accessToken = 'pk.eyJ1IjoibWFyY2hhbWJ1bCIsImEiOiI5NzI1MTA5ODk4MjFlM2EwZWIwM2Y4N2JiMjI5NTE4ZiJ9.2SFijYH9C8e_tZ2WyWboEQ';
+
+            var map = L.mapbox.map('map', 'mapbox.streets').setView([48.8, 2.35], 6);
+
+
+            // map.on('style.load', function () {
+            //
+            //     $http.get("/api/villes.json").then(function(res) {
+            //         map.addSource("markers", {
+            //             "type": "geojson",
+            //             "data": res.data
+            //         });
+            //
+            //
+            //
+            //         map.addLayer({
+            //             "id": "markers",
+            //             "type": "symbol",
+            //             "source": "markers",
+            //             "layout": {
+            //                 "icon-image": "{marker-symbol}-15",
+            //                 "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            //                 "text-offset": [0, 0.6],
+            //                 "text-anchor": "top"
+            //             },
+            //             "paint": {
+            //                 "text-size": 12
+            //             }
+            //         });
+            //
+            //         for (let tournee of res.data.features) {
+            //             var tooltip = new mapboxgl.Popup()
+            //             .setLngLat(tournee.geometry.coordinates)
+            //             .setHTML('<h4>' + tournee.properties.name + '</h4><p>' + tournee.properties.description + '</p>')
+            //             .addTo(map);
+            //         }
+            //     });
+            // });
+        };
+        this._init();
+    }
 }
 
 angular.module("marchambul").controller("MapCtrl", MapCtrl);
